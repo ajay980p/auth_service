bc0859ec6e46d56756ac1deb3d8be1717fc9a4e4
ajay980p <ajay980p@gmail.com>
2024-04-26 19:25:33 +0530

    added global error handler

--- auth_service/src/app.ts
+++ auth_service/src/app.ts
@@ -1,24 +1,32 @@
 import express, { NextFunction, Request, Response } from 'express';
 import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 // Create Express app
 const app = express();
 
 // Define routes
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    return res.send("Welcome to auth service");
 });
 
 // Define error handling middleware
-app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
 
-    try {
-        throw new Error("Something went wrong");
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            logger.error("Error log : ", error.message);
-        }
-    }
+    logger.error("Error log : ", err.message);
+
+    const statusCode = err.statusCodev || 500;
+
+    return res.status(statusCode).send({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: "",
+                location: ""
+            }
+        ]
+    });
 });
 
 // Export the Express app
--- auth_service/src/server.ts
+++ auth_service/src/server.ts
@@ -2,6 +2,7 @@
 import express, { Request, Response } from 'express';
 import { Config } from './config';
 import app from './app';
+import logger from './config/logger';
 
 // Create Express app
 const server = express();
@@ -11,5 +12,11 @@ server.use(app);
 
 // Start the server
 server.listen(Config.PORT, () => {
-    console.log(`Server is running on port ${Config.PORT}`);
+    try {
+        console.log(`Server is running on port ${Config.PORT}`);
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(`Error starting server: ${err.message}`);
+        }
+    }
 });
